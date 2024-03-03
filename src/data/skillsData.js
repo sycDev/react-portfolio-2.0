ba0c51055bd2a2d4900b224210149558c08a7fd8
sycDev <sinyi.20028@gmail.com>
2024-03-03 23:23:24 +0800

    Update skill sets

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,127 +1,139 @@
 import AxureImg from '../images/skills/axure.png';
 
 export const skillsData = {
-	ProgrammingLanguages: 
-	[
-		{
-			id: 1,
-			title: 'Java',
-			img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg',
-			link: 'https://www.java.com',
-		},
-		{
-			id: 2,
-			title: 'PHP',
-			img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg',
-			link:'https://www.php.net',
-		},
-		{
-			id: 3,
-			title: 'JavaScript',
-			img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
-			link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
-		},
-	],
+	ProgrammingLanguages:
+		[
+			{
+				id: 1,
+				title: 'Java',
+				img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original-wordmark.svg',
+				link: 'https://www.java.com',
+			},
+			{
+				id: 2,
+				title: 'PHP',
+				img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg',
+				link: 'https://www.php.net',
+			},
+			{
+				id: 3,
+				title: 'JavaScript',
+				img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
+				link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'
+			},
+			{
+				id: 4,
+				title: 'Python',
+				img: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg',
+				link: 'https://www.python.org/'
+			},
+		],
 	FrontendDevelopment:
-	[
-		{
-			id: 1,
-			title: 'HTML',
-			img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg',
-			link: 'https://www.w3.org/html'
-		},
-		{
-			id: 2,
-			title: 'CSS',
-			img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg',
-			link: 'https://www.w3.org/Style/CSS',
-		},
-		{
-			id: 3,
-			title: 'JQuery',
-			img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original-wordmark.svg',
-			link: 'https://jquery.com',
-		},
-		{
-			id: 4,
-			title: 'React',
-			img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg',
-			link: 'https://react.dev',
-		},
-		{
-			id: 5,
-			title: 'Bootstrap',
-			img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg',
-			link: 'https://getbootstrap.com',
-		},
-		{
-			id: 6,
-			title: 'Tailwind CSS',
-			img: 'https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg',
-			link: 'https://tailwindcss.com'
-		},
-		{
-			id: 7,
-			title: 'Ant Design',
-			img: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
-			link: 'https://ant.design/'
-		},
-	],
+		[
+			{
+				id: 1,
+				title: 'HTML',
+				img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original-wordmark.svg',
+				link: 'https://www.w3.org/html'
+			},
+			{
+				id: 2,
+				title: 'CSS',
+				img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg',
+				link: 'https://www.w3.org/Style/CSS',
+			},
+			{
+				id: 3,
+				title: 'JQuery',
+				img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original-wordmark.svg',
+				link: 'https://jquery.com',
+			},
+			{
+				id: 4,
+				title: 'React',
+				img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg',
+				link: 'https://react.dev',
+			},
+			{
+				id: 5,
+				title: 'Bootstrap',
+				img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original-wordmark.svg',
+				link: 'https://getbootstrap.com',
+			},
+			{
+				id: 6,
+				title: 'Tailwind CSS',
+				img: 'https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg',
+				link: 'https://tailwindcss.com'
+			},
+			{
+				id: 7,
+				title: 'Ant Design',
+				img: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+				link: 'https://ant.design/'
+			},
+			{
+				id: 8,
+				title: 'Angular',
+				img: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg',
+				link: 'https://angular.io/'
+			},
+		],
 	BackendDevelopment:
-	[
-		{
-			id: 1,
-			title: 'Spring',
-			img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original-wordmark.svg',
-			link: 'https://spring.io',
-		},
-	],
+		[
+			{
+				id: 1,
+				title: 'Spring',
+				img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original-wordmark.svg',
+				link: 'https://spring.io',
+			},
+		],
 	Databases:
-	[
-		{
-			id: 1,
-			title: 'MySQL',
-			img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg',
-			link: 'https://www.mysql.com',
-		},
-	],
+		[
+			{
+				id: 1,
+				title: 'MySQL',
+				img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original-wordmark.svg',
+				link: 'https://www.mysql.com',
+			},
+		],
 	Others:
-	[
-		{
-			id: 1,
-			title: 'Git',
-			img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg',
-			link: 'https://git-scm.com/',
-		},
-		{
-			id: 2,
-			title: 'Jira',
-			img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original-wordmark.svg',
-			link: 'https://www.atlassian.com/software/jira',
-		},
-		{
-			id: 3,
-			title: 'Postman',
-			img: 'https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg',
-			link: 'https://postman.com',
-		},
-		{
-			id: 4,
-			title: 'Axure RP',
-			img: AxureImg,
-			link: 'https://www.axure.com',
-		},
-		{
-			id: 5,
-			title: 'JUnit',
-			img: 'https://avatars.githubusercontent.com/u/874086?s=200&v=4',
-			link: 'https://junit.org/',
-		},
-		{
-			id: 6,
-			title: 'Linux',
-			img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg',
-			link: 'https://www.linux.org',
-		},
-	],
+		[
+			{
+				id: 1,
+				title: 'Git',
+				img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg',
+				link: 'https://git-scm.com/',
+			},
+			{
+				id: 2,
+				title: 'Jira',
+				img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jira/jira-original-wordmark.svg',
+				link: 'https://www.atlassian.com/software/jira',
+			},
+			{
+				id: 3,
+				title: 'Postman',
+				img: 'https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg',
+				link: 'https://postman.com',
+			},
+			{
+				id: 4,
+				title: 'Axure RP',
+				img: AxureImg,
+				link: 'https://www.axure.com',
+			},
+			{
+				id: 5,
+				title: 'JUnit',
+				img: 'https://avatars.githubusercontent.com/u/874086?s=200&v=4',
+				link: 'https://junit.org/',
+			},
+			{
+				id: 6,
+				title: 'Linux',
+				img: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linux/linux-original.svg',
+				link: 'https://www.linux.org',
+			},
+		],
 };
